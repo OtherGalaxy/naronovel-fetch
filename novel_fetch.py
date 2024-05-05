0b3a9be89009ea95be91f609b2b6c5b2a44c9b69
OtherGalaxy <109729380+OtherGalaxy@users.noreply.github.com>
2024-05-05 19:38:02 +0800

    Update novel_fetch.py
    Adapted to the latest version of the website.

--- novel_fetch.py
+++ novel_fetch.py
@@ -6,17 +6,18 @@ from urllib import request
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
 
+
 # このファイルがあるディレクトリ
 dir_base = os.path.dirname(os.path.abspath(__file__))
 
+
 def get_args():
     parser = ArgumentParser()
-    parser.add_argument("ncode", type=str,
-        help="N-code")
-    parser.add_argument("--reset", "-r", action="store_true",
-        help="delete and refetch all parts")
+    parser.add_argument("ncode", type=str, help="N-code")
+    parser.add_argument("--reset", "-r", action="store_true", help="delete and refetch all parts")
     args = parser.parse_args()
     return args
+
 
 def main():
     args = get_args()
@@ -32,13 +33,14 @@ def main():
     # 全部分数を取得
     info_url = "https://ncode.syosetu.com/novelview/infotop/ncode/{}/".format(ncode)
     try:
-        info_res = request.urlopen(info_url)
-    except Exception:
-        print("Incorrect N-code!!")
+        req = request.Request(info_url, headers={"User-Agent": "Mozilla/5.0"})
+        info_res = request.urlopen(req)
+    except Exception as e:
+        print("Error: {}".format(e))
         sys.exit(1)
     soup = BeautifulSoup(info_res, "html.parser")
     pre_info = soup.select_one("#pre_info").text
-    num_parts = int(re.search(r"全([0-9]+)部分", pre_info).group(1))
+    num_parts = int(re.search(r"全([0-9]+)エピソード", pre_info).group(1))
 
     # 小説を保存するディレクトリがなければ作成
     novel_dir = os.path.normpath(os.path.join(dir_base, "{}".format(ncode)))
@@ -52,9 +54,9 @@ def main():
     # 新たに取得すべき部分番号のリストを生成
     # resetFlagがTrueならすべての部分を取得する
     if resetFlag:
-        fetch_parts = list(range(1, num_parts+1))
+        fetch_parts = list(range(1, num_parts + 1))
     else:
-        fetch_parts = set(range(1,num_parts+1)) - existing_parts
+        fetch_parts = set(range(1, num_parts + 1)) - existing_parts
         fetch_parts = sorted(fetch_parts)
 
     num_fetch_rest = len(fetch_parts)
@@ -62,22 +64,22 @@ def main():
         # 作品本文ページのURL
         url = "https://ncode.syosetu.com/{}/{:d}/".format(ncode, part)
 
-        res = request.urlopen(url)
+        req = request.Request(url, headers={"User-Agent": "Mozilla/5.0"})
+        res = request.urlopen(req)
         soup = BeautifulSoup(res, "html.parser")
 
         # CSSセレクタで本文を指定
         honbun = soup.select_one("#novel_honbun").text
-        honbun += "\n"  # 次の部分との間は念のため改行しておく
-        
+        honbun += "\n"
+
         # 保存
         name = os.path.join(novel_dir, "{}_{:d}.txt".format(ncode, part))
         with open(name, "w", encoding="utf-8") as f:
             f.write(honbun)
-        
+
         # 進捗を表示
         num_fetch_rest = num_fetch_rest - 1
-        print("part {:d} downloaded (rest: {:d} parts)".format(
-            part, num_fetch_rest))
+        print("part {:d} downloaded (rest: {:d} parts)".format(part, num_fetch_rest))
 
         time.sleep(1)  # 次の部分取得までは1秒間の時間を空ける
 
